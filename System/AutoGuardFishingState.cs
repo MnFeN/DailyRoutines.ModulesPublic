e43ff9e53c4df770706954d6e30cf280feb7b8b8
MnFeN <self.mail.address@gmail.com>
2026-06-23 22:16:21 +0800

    Fix missing action name placeholder in module description

--- System/AutoGuardFishingState.cs
+++ System/AutoGuardFishingState.cs
@@ -6,6 +6,7 @@ using DailyRoutines.Internal;
 using FFXIVClientStructs.FFXIV.Client.Game;
 using OmenTools.Info.Game.Enums;
 using OmenTools.Interop.Game.ExecuteCommand.Implementations;
+using OmenTools.Interop.Game.Lumina;
 using OmenTools.OmenService;
 
 namespace DailyRoutines.ModulesPublic;
@@ -15,10 +16,12 @@ public class AutoGuardFishingState : ModuleBase
     public override ModuleInfo Info { get; } = new()
     {
         Title       = Lang.Get("AutoGuardFishingStateTitle"),
-        Description = Lang.Get("AutoGuardFishingStateDescription"),
+        Description = Lang.Get("AutoGuardFishingStateDescription", LuminaWrapper.GetActionName(InterruptFishingActionID)),
         Category    = ModuleCategory.System
     };
 
+    private const uint InterruptFishingActionID = 299;
+    
     public override ModulePermission Permission { get; } = new() { AllDefaultEnabled = true };
 
     protected override void Init()
@@ -47,7 +50,7 @@ public class AutoGuardFishingState : ModuleBase
         ref uint                        comboRouteID
     )
     {
-        if (actionType != ActionType.Action || actionID != 299)
+        if (actionType != ActionType.Action || actionID != InterruptFishingActionID)
             return;
 
         FishingCommand.Quit();
